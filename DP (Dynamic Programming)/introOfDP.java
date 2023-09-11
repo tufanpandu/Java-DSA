bef9ab33ddd294139656e494c6339bafe7077749
tufanpandu <tufan4344@gmail.com>
2023-09-11 22:21:47 +0530

    Add Tabulation (Bottom Up) function

--- DP (Dynamic Programming)/introOfDP.java
+++ DP (Dynamic Programming)/introOfDP.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class introOfDP {
-    // Fibonacci using DP
+    // Fibonacci using DP //👉Memoiztion (Top Down)
     public static int fib(int n, int dp[]) { // O(n)
         if (n == 0 || n == 1) {
             return n;
@@ -18,11 +18,26 @@ public class introOfDP {
         return dp[n];
     }
 
+    // Solve problem using 👉Tabulation(Bottom Up)
+    public static int tabulationFib(int n) {
+        // initialize
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
     public static void main(String[] args) {
         int n = 10;
         // Create array for storing return value for n
         int dp[] = new int[n + 1]; // Because array start from 0 index we add +1 into n.
-        System.out.println(fib(n, dp));
+        // System.out.println(fib(n, dp));
+        System.out.println(tabulationFib(n));
 
     }
 }
