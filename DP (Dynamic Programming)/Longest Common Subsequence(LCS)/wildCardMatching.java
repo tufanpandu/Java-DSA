b87adb362a73b312a9eb1f2ad179d9cb69de5b59
tufanpandu <tufan4344@gmail.com>
2023-10-30 21:46:39 +0530

    Longest Common Subsequence(LCS) Wild Card Matching

---
+++ DP (Dynamic Programming)/Longest Common Subsequence(LCS)/wildCardMatching.java
@@ -0,0 +1,19 @@
+//=========== Wild Card Matching (Hard Question) =========
+/* 
+ Given a text and wildcard pattern, implement wildcard pattern matching algorithm that finds if wildcard pattern is matched with text. The matching should cover the entire text(not partial text). The wildcard pattern can include the charcters "?" and "*"
+   - '?' - Mathches any single character
+   - '*' - Matches any sequence of characters(including the empty sequence)
+
+
+   Text = "baaabab"
+   Pattern = "**** ba****** ab"
+   output:true
+
+   Text = "baaabab"
+   Pattern = "a*ab"
+   output:false
+
+ */
+public class wildCardMatching {
+
+}
